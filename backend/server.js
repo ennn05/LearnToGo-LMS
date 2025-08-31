28a470c88eb265d01c72632477b5f93f7d98ff0c
hlee0180 <hlee0180@student.monash.edu>
2025-09-01 01:27:59 +0800

    setup: other imports and use for the server app

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,18 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import cors from 'cors';
+
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(helmet());
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Hello from backend!');
