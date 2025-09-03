02ba80263312890ef65629603148f9b8d586ef60
Yap Meng Hin <161673966+SeanYapMengHin@users.noreply.github.com>
2025-09-03 14:05:47 +0800

    Modified file structure to be able to run from the base directory
    - Moved package-lock.json and package.json out to root directory
    
    - Modified backend server.js to be connected to frontend public folder contents
    
    - Modofied backend to autommatically launch the website when run

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,14 @@ import dotenv from 'dotenv';
 import helmet from 'helmet';
 import morgan from 'morgan';
 import cors from 'cors';
+import { fileURLToPath } from 'url'; 
+import path from 'path';
+import open from 'open';
 
 dotenv.config();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -14,10 +19,14 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req, res) => {
-  res.send('Hello from backend!');
-});
+
+app.use(express.static(path.join(__dirname, "../frontend/public")));
+
+// app.get('/', (req, res) => {
+//   res.send('Hello from backend!');
+// });
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+  open(`http://localhost:${PORT}`);
 });
