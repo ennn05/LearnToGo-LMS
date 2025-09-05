3d57051bfeb548d345ce06e88ee84091424df27b
huiyeeee <116058002+huiyeeee@users.noreply.github.com>
2025-09-05 15:24:34 +0800

    refactor: use authController login function for login route
    Instead of piling up all code in authRoutes.js, create a login function in authController.js to handle login function

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -43,3 +43,28 @@ export const register = async (req, res) => {
         return res.status(500).json({ success: false, message: "Internal server error" });
     }
 };
+
+export const login = async (req, res) => {
+
+    const { email, password } = req.body;
+    console.log("Login attempt:", email, password);
+    try {
+        const instructor = await sql`SELECT * FROM "LMS".instructor WHERE instructor_email = ${email}`;
+        console.log("DB result:", instructor);
+
+        if (instructor.length > 0) {
+            if (password === instructor[0].instructor_password) {
+                return res.status(200).json({ message: "Login successful", user: instructor[0] });
+            } else {
+                return res.status(401).json({ message: "Invalid email or password" });
+            }
+        } else {
+            return res.status(401).json({ message: "Invalid email or password" });
+        }
+
+
+    } catch (error) {
+        console.error("Login error:", error);
+        return res.status(500).json({ message: "Internal server error" });
+    }
+}
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,33 +1,12 @@
 import express from "express";
-import { sql } from "../db.js";
-import { register } from "../controllers/authController.js";
+import { register, login } from "../controllers/authController.js";
 
 const router = express.Router();
 
-router.post("/login", async (req, res) => {
-  const { email, password } = req.body;
-  console.log("Login attempt:", email, password);
-
-  try {
-    const instructor = await sql`SELECT * FROM "LMS".instructor WHERE instructor_email = ${email}`;
-    console.log("DB result:", instructor);
-
-    if (instructor.length > 0) {
-      if (password === instructor[0].instructor_password) {
-        return res.status(200).json({ message: "Login successful", user: instructor[0] });
-      } else {
-        return res.status(401).json({ message: "Invalid email or password" });
-      }
-    } else {
-      return res.status(401).json({ message: "Invalid email or password" });
-    }
-  } catch (err) {
-    console.error("Login error:", err);
-    return res.status(500).json({ message: "Internal server error" });
-  }
-});
-
-
+router.post("/login", login);
 router.post("/register", register);
+
+export default router;
+
 
 export default router;
