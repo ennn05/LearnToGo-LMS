22a9fb437430a40864970cd5127722aaeb446928
hlee0180 <hlee0180@student.monash.edu>
2025-09-06 14:41:06 +0800

    feat: created other pages and establish proper Routes to all
    to ensure user needs login before able to access pages other than login/register (in frontend)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,33 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/login";
 import Dashboard from "./pages/Dashboard";
+import Lessons from "./pages/lessons";
+import Courses from "./pages/courses";
+import Students from "./pages/students";
+
+const RootLayout = () => {
+  const user = null; // Replace with actual user authentication logic
+
+  return !user ? <Navigate to="/login" replace={true} /> : (
+    <div>
+      <h1>Root Layout</h1>
+      <Outlet />
+    </div>
+  );
+}
 
 function App() {
   return (
       <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="Dashboard" element={<Dashboard />} />
+        <Route element={<RootLayout />}>
+          <Route path="/" element={<Navigate to="/courses" />} />
+          <Route path="/lessons" element={<Lessons />} />
+          <Route path="/courses" element={<Courses />} />
+          <Route path="/students" element={<Students />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
+        <Route path="/login" element={<Login />} />
+        {/* <Route path="/register" element={<Register />} /> */}
       </Routes>
   );
 }
