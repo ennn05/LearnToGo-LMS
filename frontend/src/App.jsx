3b18fe1220ea8ab74d0b2b9ae841160742d8a5bb
hlee0180 <hlee0180@student.monash.edu>
2025-09-06 14:29:04 +0800

    feat: added BrowserRouter

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,12 +4,10 @@ import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
-    <Router>
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="Dashboard" element={<Dashboard />} />
       </Routes>
-    </Router>
   );
 }
 
