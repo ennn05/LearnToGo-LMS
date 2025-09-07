b805ed99198264442e5caf1a2dc540d75514c7ca
hlee0180 <hlee0180@student.monash.edu>
2025-09-07 16:25:42 +0800

    test: temp test push

--- frontend/src/pages/CourseDetails.jsx
+++ frontend/src/pages/CourseDetails.jsx
@@ -3,6 +3,8 @@ import { useParams, useNavigate } from "react-router-dom";
 import { mockCourseAPI } from "../data/mockCourses";
 import "../styles/CourseDetails.css";
 
+// temp test
+
 function CourseDetails() {
   const { courseId } = useParams();
   const navigate = useNavigate();
