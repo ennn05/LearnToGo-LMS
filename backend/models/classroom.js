8aade0a0efbc6af2ae29619b45c2f60debe429fd
gche0093 <gche0093@student.monash.edu>
2025-09-17 02:07:44 +0800

    feat: add classroom retrive from database functionality

---
+++ backend/models/classroom.js
@@ -0,0 +1,38 @@
+import sql from "../db.js";
+
+export const getAllClassrooms = async () => {
+    const classrooms = await sql`SELECT * FROM "LMS".classroom;`;
+    return classrooms;
+}
+
+export const getClassroomsByInstructor = async (instructorId) => {
+    const classrooms = await sql`SELECT * FROM "LMS".classroom WHERE cr_creator = ${instructorId};`;
+    return classrooms;
+}
+
+export const getClassroomByCode = async (classroomCode) => {
+    //const classrooms = await sql
+}
+
+export const deleteCourse = async (classroomId) => {
+    const courses = await sql`DELETE FROM "LMS".course WHERE course_code = ${classroomId} RETURNING *;`;
+    return courses[0];
+}
+
+export const updateCourse = async (classroomData) => {
+    const {id, updateData} = classroomData;
+    console.log(classroomData);
+    const classrooms = await sql`UPDATE "LMS".classroom 
+                    SET 
+                        cr_id = ${updateData.cr_id},
+                        cr_start_date = ${updateData.cr_start_date},
+                        cr_duration = ${updateData.cr_duration},
+                        cr_date_created = ${updateData.cr_date_created},
+                        cr_last_updated = ${updateData.cr_last_updated},
+                        cr_creator = ${updateData.cr_creator},
+                        supervisor_id = ${updateData.supervisor_id},
+                        course_code = ${updateData.course_code}
+                    WHERE course_code = ${id} 
+                    RETURNING *;`;
+    return classrooms[0];
+}
