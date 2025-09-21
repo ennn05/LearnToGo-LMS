c3468b89508f5fec1a00f176d0d8b94b823b9a1b
hlee0180 <hlee0180@student.monash.edu>
2025-09-22 05:13:32 +0800

    feat: set up different lesson UI view for different role

---
+++ frontend/src/pages/StudentLessons.jsx
@@ -0,0 +1,10 @@
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import api from "../libs/apiCalls";
+import "../styles/Lessons.css";
+
+function StudentLessons() {
+    return <div>Student Lessons Page</div>;
+}
+
+export default StudentLessons;
