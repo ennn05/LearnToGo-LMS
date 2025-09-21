aa887e19d289014050a4f7cd2340e5883d73d332
hlee0180 <hlee0180@student.monash.edu>
2025-09-22 05:34:19 +0800

    feat: set up different lesson controller route for different role

--- backend/routes/lessonsRoutes.js
+++ backend/routes/lessonsRoutes.js
@@ -49,12 +49,24 @@ import {
   getLessonsByInstructor,
   addLesson,
   editLesson,
-  removeLesson
+  removeLesson,
+  getStudentLessons
 } from "../controllers/lessonControllers.js";
 
 const router = express.Router();
 
-router.get("/", getLessons);
+router.get("/", authenticate, (req, res) => {
+    switch (req?.user?.role) {
+        case "student":
+            return getStudentLessons(req, res);
+        case "instructor":
+        case "admin":
+            return getLessons(req, res);
+        default:
+            return res.status(403).json({ message: "Unauthorized" });
+    }
+});
+
 router.get("/instructor", authenticate, getLessonsByInstructor); 
 router.get("/:id", getLesson);
 router.post("/", addLesson);
