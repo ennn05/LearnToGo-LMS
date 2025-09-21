b24d149d6d1750a4b4490d5fad106152f7bde0fd
Yap Meng Hin <161673966+SeanYapMengHin@users.noreply.github.com>
2025-09-21 15:44:31 +0800

    fix: update mockstudents to better fulfill requirements

--- frontend/src/data/mockStudents.js
+++ frontend/src/data/mockStudents.js
@@ -3,16 +3,16 @@ export const mockStudents = [
   {
     stu_user_id: 1,
     stu_name: "Alice Tan",
-    stu_grade: "attended",
+    stu_grade: "pass",
   },
   {
     stu_user_id: 2,
     stu_name: "Brian Lee",
-    stu_grade: "absent",
+    stu_grade: "fail",
   },
   {
     stu_user_id: 3,
     stu_name: "Chloe Wong",
-    stu_grade: "attended",
+    stu_grade: "pass",
   },
 ];
