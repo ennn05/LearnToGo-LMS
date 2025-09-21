84f558b0d3be292a96a36658b771ec38c30c0297
hlee0180 <hlee0180@student.monash.edu>
2025-09-22 06:55:39 +0800

    test: add temp test code in FE to test backend working

--- frontend/src/pages/StudentClassrooms.jsx
+++ frontend/src/pages/StudentClassrooms.jsx
@@ -1,5 +1,17 @@
+import { useEffect } from "react";
+import api from "../libs/apiCalls";
 
 function StudentClassrooms() {
+    // for testing purpose only - replace with ur FE code
+    const fetchClassrooms = async () => {
+        const {data: res} = await api.get("classrooms");
+        console.log(res.data);
+    }
+
+    useEffect(() => {
+        fetchClassrooms();
+    }, []);
+
     return (
         <div>
             <h1>Student Classrooms</h1>
