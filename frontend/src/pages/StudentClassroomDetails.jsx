39c12885da3971340de7c904d8bd7a29fa597e95
hlee0180 <hlee0180@student.monash.edu>
2025-09-22 06:58:51 +0800

    docs: add docs for api usage in FE classroom & classroom details

--- frontend/src/pages/StudentClassroomDetails.jsx
+++ frontend/src/pages/StudentClassroomDetails.jsx
@@ -1,5 +1,7 @@
 
 function StudentClassroomDetails() {
+    // use api.get("classrooms/:classroomCode") to fetch the classroom details
+
     return (
         <div>
             <h1>Student Classroom Details</h1>
--- frontend/src/pages/StudentClassrooms.jsx
+++ frontend/src/pages/StudentClassrooms.jsx
@@ -3,6 +3,8 @@ import api from "../libs/apiCalls";
 
 function StudentClassrooms() {
     // for testing purpose only - replace with ur FE code
+
+    // use api.get("classrooms") to fetch the list of student's classrooms
     const fetchClassrooms = async () => {
         const {data: res} = await api.get("classrooms");
         console.log(res.data);
