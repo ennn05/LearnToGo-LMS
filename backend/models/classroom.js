aef20e624374233f8ebb11c45c55437e4737e652
hlee0180 <hlee0180@student.monash.edu>
2025-09-27 23:27:35 +0800

    feat: update sql for update classroom data

--- backend/models/classroom.js
+++ backend/models/classroom.js
@@ -121,9 +121,7 @@ export const updateClassroom = async (classroomData) => {
                     SET 
                         cr_start_date = ${updateData.cr_start_date},
                         cr_duration = ${updateData.cr_duration},
-                        cr_date_created = ${updateData.cr_date_created},
                         cr_last_updated = ${updateData.cr_last_updated},
-                        cr_creator = ${updateData.cr_creator},
                         supervisor_id = ${updateData.supervisor_id},
                         course_code = ${updateData.course_code},
                         cr_status = ${updateData.cr_status}
