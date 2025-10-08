04d3a25b6f17b450011771fa292d1733623f6da0
gche0093 <gche0093@student.monash.edu>
2025-10-08 13:48:01 +0800

    model : sql count total number of courses by instructor

--- backend/models/statistics.js
+++ backend/models/statistics.js
@@ -6,5 +6,8 @@ export const countTotalNumCourses = async () => {
     return courses;
 }
 
-
+export const countTotalNumCoursesByInstructor = async (instructorId) => {
+    const courses = await sql`SELECT COUNT(*) FROM "LMS".course WHERE course_creator = ${instructorId};`;
+    return courses;
+}
 
