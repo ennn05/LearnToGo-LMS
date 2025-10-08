d6eecc91090cf26b121ed142e68695d21d087199
hlee0180 <hlee0180@student.monash.edu>
2025-10-08 13:36:03 +0800

    feat/US42/T2: add backend route for course statistics abt total number of courses

--- backend/routes/statisticsRoutes.js
+++ backend/routes/statisticsRoutes.js
@@ -1,6 +1,18 @@
 import express from "express";
+import {authenticate, authorize} from "../middleware/authMiddleware";
+import { getTotalNumCourses } from "../controllers/statisticsControllers";
 
 const router = express.Router();
 
+router.get("/courses/total", authenticate, authorize("admin", "instructor"), (req, res) => {
+            switch (req?.user?.role) {
+                case "instructor":
+                    // return 
+                case "admin":
+                    return getTotalNumCourses(req, res);
+                default:
+                    return res.status(403).json({ message: "Unauthorized" });
+            }
+        });
 
 export default router;
