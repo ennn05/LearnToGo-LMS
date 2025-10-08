f4bd6116c17fbaca117897c999ed3cc750404f7c
hlee0180 <hlee0180@student.monash.edu>
2025-10-08 13:41:23 +0800

    feat/US42/T3: set up dummy sql function for countTotalNumCoursesByInstructor

--- backend/models/statistics.js
+++ backend/models/statistics.js
@@ -3,3 +3,7 @@ import sql from "../db.js";
 export const countTotalNumCourses = async () => {
     const count = 0; // dummy values for now
 }
+
+export const countTotalNumCoursesByInstructor = async (instId) => {
+    const count = 0; // dummy values for now
+}
