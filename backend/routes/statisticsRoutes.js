14c143b1d08ab061b44aa02e9075c6b6be15a9a7
hlee0180 <hlee0180@student.monash.edu>
2025-10-08 13:46:59 +0800

    feat/US42/T2: add route for getting total number of course for an instructor

--- backend/routes/statisticsRoutes.js
+++ backend/routes/statisticsRoutes.js
@@ -1,13 +1,13 @@
 import express from "express";
 import {authenticate, authorize} from "../middleware/authMiddleware";
-import { getTotalNumCourses } from "../controllers/statisticsControllers";
+import { getTotalNumCourses, getTotalNumCoursesOfInstructor } from "../controllers/statisticsControllers";
 
 const router = express.Router();
 
 router.get("/courses/total", authenticate, authorize("admin", "instructor"), (req, res) => {
             switch (req?.user?.role) {
                 case "instructor":
-                    // return 
+                    return getTotalNumCoursesOfInstructor(req, res);
                 case "admin":
                     return getTotalNumCourses(req, res);
                 default:
