405ff7af537a886ec1afbdd66100d981589bc4c0
hlee0180 <hlee0180@student.monash.edu>
2025-10-08 13:45:17 +0800

    feat/US42/T3: add backend controller for getTotalNumCoursesOfInstructor

--- backend/controllers/statisticsControllers.js
+++ backend/controllers/statisticsControllers.js
@@ -1,8 +1,30 @@
-import { countTotalNumCourses } from "../models/statistics";
+import { countTotalNumCourses, countTotalNumCoursesByInstructor } from "../models/statistics";
 
 export const getTotalNumCourses = async (req, res) => {
     try {
         const count = await countTotalNumCourses();
+
+        if (count === null || count === undefined) {
+            return res.status(404).json({
+                success: false,
+                message: "Course statistics not found.",
+            });
+        }
+        
+        return res.status(200).json({ success: true, data: count });
+    }
+    catch (error) {
+        console.error("Error fetching total number of courses:", error);
+        return res.status(500).json({ success: false, message: "Failed to fetch total number of courses." });
+    }
+}
+
+export const getTotalNumCoursesOfInstructor = async (req, res) => {
+    const instructorId = req?.user?.id;
+    if (!instructorId) return res.status(401).json({ success: false, message: "Unauthorized" });
+
+    try {
+        const count = await countTotalNumCoursesByInstructor(instructorId);
 
         if (count === null || count === undefined) {
             return res.status(404).json({
