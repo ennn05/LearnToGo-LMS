220c0e89b843e3d88877499b6cd5878c9d2b16a9
hlee0180 <hlee0180@student.monash.edu>
2025-10-08 14:26:00 +0800

    feat/US42/T5: direct instructor fetch avg lessons api route to getAvgLessonsPerCourseOfInstructor

--- backend/routes/statisticsRoutes.js
+++ backend/routes/statisticsRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import {authenticate, authorize} from "../middleware/authMiddleware";
-import { getTotalNumCourses, getTotalNumCoursesOfInstructor } from "../controllers/statisticsControllers";
+import { getAvgLessonsPerCourseOfInstructor, getTotalNumCourses, getTotalNumCoursesOfInstructor } from "../controllers/statisticsControllers";
 
 const router = express.Router();
 
@@ -18,7 +18,7 @@ router.get("/courses/total", authenticate, authorize("admin", "instructor"), (re
 router.get("/courses/average-lessons", authenticate, authorize("admin", "instructor"), (req, res) => {
             switch (req?.user?.role) {
                 case "instructor":
-                    // return 
+                    return getAvgLessonsPerCourseOfInstructor(req, res);
                 case "admin":
                     return getAvgLessonsPerCourse(req, res);
                 default:
