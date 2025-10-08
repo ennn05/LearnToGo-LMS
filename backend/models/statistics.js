cf76d104d14020624df64d28bf49d12a901e91a9
gche0093 <gche0093@student.monash.edu>
2025-10-08 14:37:40 +0800

    model : fix count total num courses by instructor to support published, draft and archived

--- backend/models/statistics.js
+++ backend/models/statistics.js
@@ -7,7 +7,18 @@ export const countTotalNumCourses = async () => {
 }
 
 export const countTotalNumCoursesByInstructor = async (instructorId) => {
-    const courses = await sql`SELECT COUNT(*) FROM "LMS".course WHERE course_creator = ${instructorId};`;
+    const courses = await sql`
+    SELECT
+    CASE 
+        WHEN course_status = 'published' THEN 'Published'
+        WHEN course_status = 'draft' THEN 'Draft'
+        WHEN course_status = 'archived' THEN 'Archived'
+        ELSE 'Null'
+    END AS status,
+    COUNT(*) AS course_count
+    FROM "LMS".course 
+    WHERE course_creator = ${instructorId}
+    GROUP BY status;`;
     return courses;
 }
 
