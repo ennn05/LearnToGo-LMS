42da3e72dd2da892de86ccdccceb5d26bb7d197c
hlee0180 <hlee0180@student.monash.edu>
2025-10-12 21:51:56 +0800

    feat/US43/T2: add api route for get total lessons request

--- backend/routes/statisticsRoutes.js
+++ backend/routes/statisticsRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import {authenticate, authorize} from "../middleware/authMiddleware.js";
-import { getAvgLessonsPerCourse, getAvgLessonsPerCourseOfInstructor, getNumCoursesBreakdownByStatus, getNumCoursesBreakdownByStatusForInstructor, getTotalNumCourses, getTotalNumCoursesOfInstructor } from "../controllers/statisticsControllers.js";
+import { getAvgLessonsPerCourse, getAvgLessonsPerCourseOfInstructor, getNumCoursesBreakdownByStatus, getNumCoursesBreakdownByStatusForInstructor, getTotalNumCourses, getTotalNumCoursesOfInstructor, getTotalNumLessons, getTotalNumLessonsOfInstructor } from "../controllers/statisticsControllers.js";
 
 const router = express.Router();
 
@@ -36,5 +36,17 @@ router.get("/courses/status-breakdown", authenticate, authorize("admin", "instru
                     return res.status(403).json({ message: "Unauthorized" });
             }
         });
+
+router.get("/lessons/total", authenticate, authorize("admin", "instructor"), (req, res) => {
+            switch (req?.user?.role) {
+                case "instructor":
+                    return getTotalNumLessonsOfInstructor(req, res);
+                case "admin":
+                    return getTotalNumLessons(req, res);
+                default:
+                    return res.status(403).json({ message: "Unauthorized" });
+            }
+        });
+
 
 export default router;
