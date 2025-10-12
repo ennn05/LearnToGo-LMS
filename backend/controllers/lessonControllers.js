c9d15f193cbd55792269e385e18117da24613ebe
hlee0180 <hlee0180@student.monash.edu>
2025-10-12 22:17:36 +0800

    feat/US40/T3: add controller for request to get details of student's lesson
    including student's grade & completion of that lesson

--- backend/controllers/lessonControllers.js
+++ backend/controllers/lessonControllers.js
@@ -1,4 +1,4 @@
-import {getAllLessons, getLessonById, getLessonByInstructor, updateLesson, deleteLesson, createLesson, getLessonsByStudent, getPublishedLessons } from "../models/lesson.js";
+import {getAllLessons, getLessonById, getLessonByInstructor, updateLesson, deleteLesson, createLesson, getLessonsByStudent, getPublishedLessons, getStudentLessonById } from "../models/lesson.js";
 export const getLessons = async (req, res) => {
     try {
         const lessons = await getAllLessons();
@@ -15,6 +15,22 @@ export const getLesson = async (req, res) => {
     const { id } = req.params;
     try {
         const lesson = await getLessonById(id);
+        if (lesson) {
+            return res.status(200).json({ success: true, data: lesson });
+        }
+        return res.status(404).json({ success: false, message: "Lesson not found." });
+    }
+    catch (error) {
+        console.error("Error fetching lesson:", error);
+        return res.status(500).json({ success: false, message: "Failed to fetch lesson." });
+    }
+};
+
+
+export const getStudentLesson = async (req, res) => {
+    const { id } = req.params;
+    try {
+        const lesson = await getStudentLessonById(id);
         if (lesson) {
             return res.status(200).json({ success: true, data: lesson });
         }
