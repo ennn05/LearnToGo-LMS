66a260d9af31f4d3287acf8f38903b61e76dadbc
hlee0180 <hlee0180@student.monash.edu>
2025-10-12 21:51:19 +0800

    feat/US43/T3: add controller for instructor request for total num of lessons

--- backend/controllers/statisticsControllers.js
+++ backend/controllers/statisticsControllers.js
@@ -144,3 +144,24 @@ export const getTotalNumLessons = async (req, res) => {
     }
 }
 
+export const getTotalNumLessonsOfInstructor = async (req, res) => {
+    const instructorId = req?.user?.id;
+    if (!instructorId) return res.status(401).json({ success: false, message: "Unauthorized" });
+
+    try {
+        const count = await countTotalNumLessonsByInstructor(instructorId);
+
+        if (count === null || count === undefined) {
+            return res.status(404).json({
+                success: false,
+                message: "Lesson statistics not found.",
+            });
+        }
+        
+        return res.status(200).json({ success: true, data: count });
+    }
+    catch (error) {
+        console.error("Error fetching total number of lessons:", error);
+        return res.status(500).json({ success: false, message: "Failed to fetch total number of lessons." });
+    }
+}
