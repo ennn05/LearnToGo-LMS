c9c63425b9cc14dcd25beaa86cbf2031e713c1c6
hlee0180 <hlee0180@student.monash.edu>
2025-10-14 15:32:32 +0800

    fix/US42: use back original sql code

--- backend/models/statistics.js
+++ backend/models/statistics.js
@@ -8,27 +8,18 @@ export const countTotalNumCoursesByInstructor = async (instructorId) => {
 
 export const numCourseBreakdownByStatusForInstructor = async (instructorId) => {
     const courses = await sql`
-            WITH status_list AS (
-                SELECT * FROM (VALUES ('Published'), ('Draft'), ('Archived')) AS s(status)
-            ),
-            course_counts AS (
-                SELECT
-                    CASE 
-                        WHEN course_status = 'published' THEN 'Published'
-                        WHEN course_status = 'draft' THEN 'Draft'
-                        WHEN course_status = 'archived' THEN 'Archived'
-                    END AS status,
-                    COUNT(*) AS course_count
-                FROM "LMS".course 
-                WHERE course_creator = ${instructorId}
-                GROUP BY status
-            )
-            SELECT 
-                status_list.status,
-                COALESCE(course_counts.course_count, 0) AS course_count
-            FROM status_list
-            LEFT JOIN course_counts ON status_list.status = course_counts.status;
-        `;
+        SELECT
+        CASE 
+            WHEN course_status = 'published' THEN 'Published'
+            WHEN course_status = 'draft' THEN 'Draft'
+            WHEN course_status = 'archived' THEN 'Archived'
+            ELSE 'Null'
+        END AS status,
+        COUNT(*) AS course_count
+        FROM "LMS".course 
+        WHERE course_creator = ${instructorId}
+        GROUP BY status;`;
+
     return courses;
 }
 
