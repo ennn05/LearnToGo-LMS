21ea41194fbb4e12de8c51fe37964bc4ab85c40a
Yap Meng Hin <myap0029@student.monash.edu>
2025-10-16 16:09:28 +0800

    fix: fixed backend bug that fetches user statistics wrongly

--- backend/routes/userStatisticsRoutes.js
+++ backend/routes/userStatisticsRoutes.js
@@ -1,15 +1,22 @@
 import express from "express";
 import { getUserStatistics } from "../controllers/userStatisticsController.js";
-import { verifyToken, authorizeRoles } from "../middleware/auth.js"; 
-// assuming you have middleware for JWT + role checking
+// import { verifyToken, authorizeRoles } from "../middleware/auth.js"; 
+// // assuming you have middleware for JWT + role checking
+import { authenticate, authorize } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 // GET /api/statistics/users
+// router.get(
+//   "/users",
+//   verifyToken,
+//   authorizeRoles("admin", "instructor"), // block students
+//   getUserStatistics
+// );
 router.get(
   "/users",
-  verifyToken,
-  authorizeRoles("admin", "instructor"), // block students
+  authenticate,
+  authorize("admin", "instructor"),
   getUserStatistics
 );
 
