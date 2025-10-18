8aab2d14b9be6c50b5ebedede2bfffa0cbf1edc3
ennn12 <jkam0017@student.monash.edu>
2025-10-18 22:50:19 +0800

    fix: fix userStatisticRoutes bugs

--- backend/routes/userStatisticsRoutes.js
+++ backend/routes/userStatisticsRoutes.js
@@ -1,22 +1,10 @@
 import express from "express";
 import { getUserStatistics } from "../controllers/userStatisticsController.js";
-import { authenticate, authorize } from "../middleware/authMiddleware.js";
+import { authorize } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-// GET /api/statistics/users
-// router.get(
-//   "/users",
-//   verifyToken,
-//   authorizeRoles("admin", "instructor"), // block students
-//   getUserStatistics
-// );
-
-router.get("/users", authenticate, authorize("admin", "instructor"),
-  (req, res) => { 
-    console.log("HITTTT")
-    return getUserStatistics(req, res);
-  }
-);
+// Only Admins & Instructors can access
+router.get("/users", authorize("admin", "instructor"), getUserStatistics);
 
 export default router;
