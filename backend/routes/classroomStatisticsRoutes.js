fd740581c2390d121ef87a12a4cb9ebd47e566b9
ennn12 <jkam0017@student.monash.edu>
2025-10-20 01:24:25 +0800

    fix/US44: admin couldnt access

--- backend/routes/classroomStatisticsRoutes.js
+++ backend/routes/classroomStatisticsRoutes.js
@@ -6,7 +6,6 @@ import {
   getClassroomStatusBreakdown,
   getClassroomStatusBreakdownByInstructor,
   getAvgStudentsPerClassroom, 
-  getAvgStudentsPerClassroomByInstructor,
 } from "../controllers/classroomStatisticsControllers.js";
 
 const router = express.Router();
